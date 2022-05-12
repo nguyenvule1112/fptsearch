f05e80a7a98ce87bb0462e16f0f4b68b75ce17be
nguyenvule1112 <nguyenvule1112@gmail.com>
2022-05-12 16:23:06 +0700

    new update12_5

--- src/main/java/org/example/fptsearch.java
+++ src/main/java/org/example/fptsearch.java
@@ -11,15 +11,18 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import javax.swing.*;
 import java.time.Duration;
 import java.util.List;
+import java.util.Random;
 
 public class fptsearch {
     WebDriver driver;
-    String keyword = "iPhone 13 Pro Max";
+    String keyword = "iPhone 12 Pro Max";
     @Before
     public void before(){
         WebDriverManager.chromedriver().setup();
@@ -29,7 +32,7 @@ public class fptsearch {
         this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
     }
 
-//    @After
+    //    @After
 //    public void after() throws InterruptedException {
 //        Thread.sleep(3000);
 //        this.driver.quit();
@@ -62,57 +65,78 @@ public class fptsearch {
         Assert.assertTrue(isIncludeKeyword);
 
 
-    //3 bài viết tìm nhiều
-    List<WebElement> lstHitnew = this.driver.findElements(By.xpath("//a[@class=\"news-js\"]"));
+        //3 bài viết tìm nhiều
+        List<WebElement> lstHitnew = this.driver.findElements(By.xpath("//a[@class=\"news-js\"]"));
         Assert.assertEquals(lstHitnew.size(), 3);
 
         for (WebElement tl: lstHitnew) {
-        String itemName = tl.getText();
-        if(!itemName.contains(this.keyword))
-        {
-            isIncludeKeyword = false;
-            break;
+            String itemName = tl.getText();
+            if(!itemName.contains(this.keyword))
+            {
+                isIncludeKeyword = false;
+                break;
+            }
         }
-    }
         Assert.assertTrue(isIncludeKeyword);
-}
-
-
-    public boolean elementExists(By locator)
-    {
-        return !this.driver.findElements(locator).isEmpty();
     }
-
-   public boolean check_button() {
-        boolean isExist = this.elementExists(By.cssSelector("div.c-comment-loadMore>a"));
-        if (isExist) {
-            WebElement btloadmore = this.driver.findElement(By.cssSelector("div.c-comment-loadMore>a"));
-            return btloadmore.isEnabled();
-        }
-        return false;
-   }
 
     @Test
-    public void list_with_keyword() throws InterruptedException {
+    public void list_with_keyword_and_8_product_per_page() throws InterruptedException {
         //webelement
         WebElement fsearch = this.driver.findElement(By.cssSelector("input[class=\"fs-stxt\"]"));
         fsearch.sendKeys(this.keyword);
         fsearch.sendKeys(Keys.ENTER);
         this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
-        WebDriverWait waitSuggestion = new WebDriverWait(this.driver, Duration.ofSeconds(15));
+        WebElement lbTotal = this.driver.findElement(By.xpath("(//h1[contains(@class, \"fs-search-result\")]/span)[1]"));
+        int totalItems = Integer.parseInt(lbTotal.getText());
+        int totalPage = (int)Math.ceil((double) totalItems/(double) 8);
 
+        WebDriverWait waitLoadMore = new WebDriverWait(this.driver, Duration.ofSeconds(15));
+        for (int i = 0; i < totalPage-1; i++) {
+            WebElement btnLoadMore = waitLoadMore.until(
+                    ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.c-comment-loadMore>a"))
+            );
+            btnLoadMore.click();
+        }
 
-        while (check_button()){
-            boolean isExist = this.elementExists(By.cssSelector("div.c-comment-loadMore>a"));
-            if (isExist) {
-                WebElement btloadmore = waitSuggestion.until(
-                        ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.c-comment-loadMore>a")));
-                btloadmore.click();
+        Thread.sleep(2000);
+        //Check danh sách bao gồm tên sản phẩm
+        List<WebElement> nameprd = this.driver.findElements(By.cssSelector("div.cdt-product__info>h3>a"));
+        Assert.assertEquals(nameprd.size(), totalItems);
+
+        boolean isIncludeKeyword = true;
+        for (WebElement tl: nameprd) {
+            String itemName = tl.getAttribute("title");
+            if(!itemName.contains(this.keyword))
+            {
+                isIncludeKeyword = false;
+                break;
             }
-//            Thread.sleep(500);
-            if (!check_button()) {break;}
+        }
+        Assert.assertTrue(isIncludeKeyword);
 
-        };
+        //Kiem tra tong so item hien thi ket qua co = 51 hay khong?
+//        List<WebElement> lstProduct = this.driver.findElements(By.xpath("//div[contains(@class, \"row-flex\")]/div[contains(@class, \"cdt-product\")]"));
+//
+//        Assert.assertEquals(totalItems, lstProduct.size());
+    }
+
+    @Test
+    public void buy(){
+        WebElement fsearch = this.driver.findElement(By.cssSelector("input[class=\"fs-stxt\"]"));
+        fsearch.sendKeys(this.keyword);
+        fsearch.sendKeys(Keys.ENTER);
+        this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        List<WebElement> lstproduct = this.driver.findElements(By.cssSelector("div.cdt-product"));
+        Random rdn = new Random();
+        int buyIndex = rdn.nextInt(lstproduct.size());
+
+        Actions mouseaction = new Actions(this.driver);
+        mouseaction.moveToElement(lstproduct.get(buyIndex)).perform();
+
+        WebElement buynow = this.driver.findElement(By.xpath("//a[text()=\"MUA NGAY\"]"));
+        mouseaction.moveToElement(buynow).click().perform();
     }
 }
